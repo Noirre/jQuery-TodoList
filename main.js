cdc595407471adb3e4c5da256beec9e923c09e72
Alvin <alvin.qin.ca@gmail.com>
2019-06-09 13:35:16 -0700

    Fixed click checkbox

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
 $('#btn').click(function () {
-    var todo = $('#todo').val()
-    $("#todo").val("")
-    if (todo != "") {
-      var todoList = $("#todo-list")
-  
-      var newTodo = $("<li>")
-      var todoLable = $("<lable>")
-      todoLable.text(todo)
-      newTodo.append(todoLable)
+  var todo = $('#todo').val()
+  $("#todo").val("")
+  if (todo != "") {
+    var todoList = $("#todo-list")
 
-      var checkBox = $('<input type="checkbox" /> ')
-  
-      var RemoveBtn = $("<button>")
-      RemoveBtn.text("Remove")
-  
-      RemoveBtn.click(function () {
-        newTodo.remove()
-      })
-      newTodo.prepend(checkBox)
-      todoList.prepend(newTodo)
-      
-      if(checkBox.prop("checked")) {
-        debugger
+    var newTodo = $("<li>")
+    var todoLable = $("<lable>")
+    todoLable.text(todo)
+    newTodo.append(todoLable)
+
+    var checkBox = $('<input type="checkbox" id = "check-box"/> ')
+
+    var RemoveBtn = $("<button>")
+    RemoveBtn.text("Remove")
+
+    RemoveBtn.click(function () {
+      newTodo.remove()
+    })
+    newTodo.prepend(checkBox)
+    todoList.prepend(newTodo)
+
+    checkBox.click(function () {
+      if (checkBox.prop("checked")) {
         todoLable.addClass("need-cross");
       }
-      newTodo.append(RemoveBtn)
-    }
-  })
-  
-  $("#clear-btn").click(function () {
-    var todoList = $('#todo-list')
-    todoList.empty()
-  })
-    $("h2").css("color", "yellow");
-  
+    })
+
+
+    newTodo.append(RemoveBtn)
+  }
+})
+
+
+
+
+$("#clear-btn").click(function () {
+  var todoList = $('#todo-list')
+  todoList.empty()
+})
+$("h2").css("color", "yellow");
